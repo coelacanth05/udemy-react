746f21c75e1f88f9ea2d557c6689676f9d0fe1e8
kai <63350063+coelacanth05@users.noreply.github.com>
2021-05-10 21:26:45 +0900

    ４１

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 import React from "react";
 import ReactDom from "react-dom";
+import App from "./App"
 
-const App = () => {
-  return (
-    <>
-      <h1>hello</h1>
-      <p>world</p>
-    </>
-  );
-};
+
 
 ReactDom.render(<App />, document.getElementById("root"));
