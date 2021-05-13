dcd4fddab2769adcdda4503bb6bc7b9f6265d39b
kai <63350063+coelacanth05@users.noreply.github.com>
2021-05-13 21:23:25 +0900

    43

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
 const App = () => {
+  const onClickButton = () => alert();
   return (
     <>
-      <h1>hello</h1>
+      <h1 style = {{color: 'red'}}>hello</h1>
       <p>world</p>
+      <button onClick={onClickButton}>ボタン</button>
     </>
   );
 };
