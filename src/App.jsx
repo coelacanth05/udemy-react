410f1017a138bc8e38580f43803ab3b09731a521
kai <63350063+coelacanth05@users.noreply.github.com>
2021-05-19 22:02:54 +0900

    use

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,35 @@
-import React, {useState} from "react";
+import React, {useEffect, useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
+
   const onClickCountUp = () => {
-setNum(num + 1);
+    setNum(num + 1);
   };
-  const [num,setNum] = useState(0);
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag)
+  };
+
+  if (num > 0){
+  if (num % 3 === 0){
+faceShowFlag || setFaceShowFlag(true);
+  }else {
+faceShowFlag && setFaceShowFlag(false);
+  }
+}
+
   return (
     <>
       <h1 style={{ color: "red" }}>hello</h1>
       <ColorfulMessage color="blue">wrold</ColorfulMessage>
       <ColorfulMessage color="pink">wrold?</ColorfulMessage>
-      <button onClick={onClickCountUp}>カウントアップ</button>
+      <button onClick={onClickCountUp}>世界の〇〇</button>
+      <br />
+      {/* <button onClick={onClickSwitchShowFlag}>on/off</button> */}
       <p>{num}</p>
+      {faceShowFlag && <p>(・Д・)</p>}
     </>
   );
 };
