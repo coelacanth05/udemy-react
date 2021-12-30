21d37258af8e73013c92aea28bc6d6466c866b01
kai <63350063+coelacanth05@users.noreply.github.com>
2021-12-30 21:57:55 +0900

    end end

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,40 @@
 // import { InlineStyles } from "./components/InlineStyles";
 // import { StyledComponents } from "./components/StyledComponents";
 // import { StyledJsx } from "./components/StyledJsx";
-import { TailwindCss } from "./components/TailwindCss";
+// import { TailwindCss } from "./components/TailwindCss";
+
+// export const App = () => {
+//   return (
+//     <div>
+//       {/* <InlineStyles /> */}
+//       {/* <CssModules /> */}
+//       {/* <StyledJsx /> */}
+//       {/* <StyledComponents /> */}
+//       {/* <Emotion /> */}
+//       <TailwindCss />
+//     </div>
+//   );
+// };
+// 6章
+import { useState } from "react";
+import { Child1 } from "./components/Child1";
+import { Child4 } from "./components/Child4";
 
 export const App = () => {
+  console.log("Appレンダリング");
+
+  const [num, setNum] = useState(0);
+
+  const onClickButton = () => {
+    setNum(num + 1);
+  };
+
   return (
-    <div>
-      {/* <InlineStyles /> */}
-      {/* <CssModules /> */}
-      {/* <StyledJsx /> */}
-      {/* <StyledComponents /> */}
-      {/* <Emotion /> */}
-      <TailwindCss />
-    </div>
+    <>
+      <button onClick={onClickButton}>ボタン</button>
+      <p>{num}</p>
+      <Child1 />
+      <Child4 />
+    </>
   );
 };
